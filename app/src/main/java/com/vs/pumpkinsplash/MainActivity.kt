b74fbc6173204b287d83f5653c7494740218fa10
vyom <vyom48901@gmail.com>
2025-11-07 19:48:16 +0530

    splashscreen

---
+++ app/src/main/java/com/vs/pumpkinsplash/MainActivity.kt
@@ -0,0 +1,64 @@
+package com.vs.pumpkinsplash
+
+import android.animation.ObjectAnimator
+import android.os.Bundle
+import android.view.View
+import android.view.animation.OvershootInterpolator
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.animation.doOnEnd
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.lifecycleScope
+import com.vs.pumpkinsplash.ui.theme.PumpkinSplashTheme
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        var showSplash = true
+        lifecycleScope.launch {
+            delay(5000L)
+            showSplash = false
+        }
+
+        installSplashScreen().setKeepOnScreenCondition {
+            showSplash
+        }
+        setContent {
+            PumpkinSplashTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    PumpkinSplashTheme {
+        Greeting("Android")
+    }
+}
